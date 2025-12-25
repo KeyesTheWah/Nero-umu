0177ff9f4420d7f0326512058f4803b8a98ca922
Keyes <blooddimond61@gmail.com>
2025-12-24 23:50:27 -0800

    remove unused arg

--- src/nerofs.h
+++ src/nerofs.h
@@ -86,7 +86,6 @@ public:
         QStringList cachyOsOptions = {
             "UseNvidiaLibs",
             "ImageReconstructionIndicator",
-            "UseLocalShaderCache",
             "SteamInputDisabled",
             "NoWindowDecorations",
         };
